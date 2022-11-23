a26bd001baf2fb1d36d4913141074c3c367b3008
Nolan Nordwall <NolanNordwall@gmail.com>
2022-11-23 10:59:10 -0800

    Redesigned pokemon. Added more information to each pokemon

--- Lab 11/src/Pokemon.js
+++ Lab 11/src/Pokemon.js
@@ -9,7 +9,14 @@ function Pokemon({ pokemon }) {
 
   return (
     <>
+    <div className="pokemon">
       <img src={`https://github.com/fanzeyi/pokemon.json/raw/master/images/${getThreeDigitId(pokemon.id)}.png`} />
+      <div className="info">
+        <span className="number">#{getThreeDigitId(pokemon.id)}</span>
+        <h3 className="name">{pokemon.name.english}</h3>
+        <small className="type">Type: <span>{pokemon.type.join(' | ')}</span></small>
+      </div>
+    </div>
     </>
   )
 }
